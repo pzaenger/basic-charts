3fb2a96088d0ecc61fb470ba469e1dc115da2c6f
pzaenger <dev@pzaenger.com>
2019-11-20 23:03:41 +0100

    fixed formatting

--- src/index.ts
+++ src/index.ts
@@ -8,38 +8,3 @@ export { IValidations } from './interfaces/validations';
 
 // Misc
 export { validations } from './utils/validations';
-
-// Shortcuts
-// export async function dataToSVG(): Promise<string> {
-//   return '';
-// }
-//
-// export async function dataToHTML(): Promise<string> {
-//   return '';
-// }
-//
-// export async function dataToDataURL(): Promise<string> {
-//   return '';
-// }
-
-// (async () => {
-//   try {
-//     const chart = new Barchart({
-//       height: 600,
-//       width: 600,
-//       padding: 5,
-//       theme: 'default',
-//       title: 'Chart'
-//     });
-//
-//     const dataURL = await chart.toDataURL();
-//     const html = chart.toHTML();
-//     const svg = await chart.toSVG();
-//
-//     console.log(dataURL);
-//     console.log(html);
-//     console.log(svg);
-//   } catch (e) {
-//     throw e;
-//   }
-// })().catch((reason) => console.error(reason));
